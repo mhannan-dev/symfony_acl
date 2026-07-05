2b4015114164ef37c05ba07adc76e53fffb1fd89
Muhammad Hannan <hannan@redlimesolutions.com>
2026-07-05 16:40:53 +0600

    feat: initialize project with Symfony dependencies, build assets, and initial configuration

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -2,11 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Rompetomp\InertiaBundle\InertiaBundle::class => ['all' => true],
-    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
